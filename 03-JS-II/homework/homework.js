ed79cee4619df02c9207377df1d4ce7b140d4a97
saurius <108759388+saurius@users.noreply.github.com>
2022-08-02 13:42:24 -0500

    Update homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -22,12 +22,21 @@ function mayoriaDeEdad(edad) {
     return "Not allowed";
   }
   
+  }
+  
 function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   //Cuando el estado es igual a 1, el usuario está "Online"
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status == 1){
+    return "online";
+  }else if (status == 2){
+    return "away";
+  }else if (status!= 1,2){
+    return "offline";
+  }
 }
 
 function saludo(idioma) {
@@ -37,6 +46,17 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma == "aleman"){
+    return "Guten Tag!";
+  }else if(idioma == "mandarin"){
+    return "Ni Hao!";
+  }else if(idioma == "ingles"){
+  return "hello";
+  }else if(idioma == "undefined"){
+    return "hola";
+  }else {
+    return "hola";
+  }
 }
 
 function colors(color) {
